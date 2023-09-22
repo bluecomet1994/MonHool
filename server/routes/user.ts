d565adba5c1900f6d0c7ddc10d2382a8a58063d8
Morgan Mastrangelo <morgan@drivingoinc.com>
2023-09-21 23:22:59 -0400

    updated package

--- server/routes/user.ts
+++ server/routes/user.ts
@@ -6,7 +6,7 @@ import LoginValidationSchema from '../validations/LoginValidationSchema';
 
 const router: express.Router = express.Router();
 
-router.get('/current', UserController.test)
+router.post('/current', UserController.test)
 router.post('/register', RegisterValidationSchema, UserController.register);
 router.post('/login', LoginValidationSchema, UserController.login);
 router.get('/access-token', passport.authenticate('jwt', {session: false}), UserController.accessToken);
